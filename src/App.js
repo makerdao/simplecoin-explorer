b8aef9fa783d132c7185b1d69155a5ff0510e3f0
Mariano Conti <nanexcool@gmail.com>
2016-10-21 12:06:57 -0300

    Check accounts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    let web3 = new Web3(window.web3 ? window.web3.currentProvider : (
+      new Web3.providers.HttpProvider("http://localhost:8545")
+    ))
+    this.web3 = web3;
+    this.state = {
+      accounts: null,
+      defaultAccount: null
+    }
+    this.checkAccounts = this.checkAccounts.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkAccounts();
+    
+    const intervalId = setInterval(this.checkAccounts, 5000);
+    // store intervalId in the state so it can be accessed later:
+    this.setState({intervalId});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  checkAccounts() {
+    this.web3.eth.getAccounts((error, accounts) => {
+      if (!error) {
+        this.setState({
+          accounts,
+          defaultAccount: accounts[0]
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +49,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+          Account: {this.state.defaultAccount}
         </p>
       </div>
     );
