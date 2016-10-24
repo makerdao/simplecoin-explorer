750551d59e56a050997c27255e308404dabdbfd6
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2016-10-24 13:54:17 -0300

    Adding sub variable in state

--- src/components/App.js
+++ src/components/App.js
@@ -14,17 +14,19 @@ class App extends Component {
     ))
     this.web3 = web3;
     this.state = {
-      syncing: null,
-      startingBlock: null,
-      currentBlock: null,
-      highestBlock: null,
-      latestBlock: null,
-      outOfSync: null,
-      isConnected: null,
-      network: null,
-      accounts: null,
-      defaultAccount: null
-    }
+      network: {
+        syncing: null,
+        startingBlock: null,
+        currentBlock: null,
+        highestBlock: null,
+        latestBlock: null,
+        outOfSync: null,
+        isConnected: null,
+        network: null,
+        accounts: null,
+        defaultAccount: null
+      }
+    };
 
     this.checkNetwork = this.checkNetwork.bind(this);
     this.initNetwork = this.initNetwork.bind(this);
@@ -33,9 +35,9 @@ class App extends Component {
 
     this.web3.eth.isSyncing((error, sync) => {
       if (!error) {
-        this.setState({
-          syncing: sync !== false
-        });
+        const networkState = {...this.state.network};
+        networkState['syncing'] = (sync !== false);
+        this.setState({ network: networkState });
 
         // Stop all app activity
         if (sync === true) {
@@ -44,15 +46,15 @@ class App extends Component {
           this.checkNetwork();
         // show sync info
         } else if (sync) {
-          this.setState({
-            startingBlock: sync.startingBlock,
-            currentBlock: sync.currentBlock,
-            highestBlock: sync.highestBlock,
-          });
+          const networkState = {...this.state.network};
+          networkState['startingBlock'] = sync.startingBlock;
+          networkState['currentBlock'] = sync.currentBlock;
+          networkState['highestBlock'] = sync.highestBlock;
+          this.setState({ network: networkState });
         } else {
-          this.setState({
-            outOfSync: false
-          });
+          const networkState = {...this.state.network};
+          networkState['outOfSync'] = false;
+          this.setState({ network: networkState });
         }
       }
     });
@@ -71,15 +73,15 @@ class App extends Component {
     const checkAccountsInterval = setInterval(this.checkAccounts, 10000);
     const checkNetworkInterval = setInterval(this.checkNetwork, 3000);
     // store checkAccountsInterval in the state so it can be accessed later:
-    this.setState({
-      checkAccountsInterval, 
-      checkNetworkInterval
-    });
+    const networkState = {...this.state.network};
+    networkState['checkAccountsInterval'] = checkAccountsInterval;
+    networkState['checkNetworkInterval'] = checkNetworkInterval;
+    this.setState({ network: networkState });
   }
 
   componentWillUnmount() {
-    clearInterval(this.state.checkAccountsInterval);
-    clearInterval(this.state.checkNetworkInterval);
+    clearInterval(this.state.network.checkAccountsInterval);
+    clearInterval(this.state.network.checkNetworkInterval);
   }
 
   checkNetwork() {
@@ -89,11 +91,11 @@ class App extends Component {
       // Check if we are synced
       if (isConnected) {
         this.web3.eth.getBlock('latest', (e, res) => {
-          if (res.number >= this.state.latestBlock) {
-            this.setState({
-              latestBlock: res.number,
-              outOfSync: e != null || ((new Date().getTime() / 1000) - res.timestamp) > 600 
-            });
+          if (res.number >= this.state.network.latestBlock) {
+            const networkState = {...this.state.network};
+            networkState['latestBlock'] = res.number;
+            networkState['outOfSync'] = e != null || ((new Date().getTime() / 1000) - res.timestamp) > 600;
+            this.setState({ network: networkState });
           } else {
             // XXX MetaMask frequently returns old blocks
             // https://github.com/MetaMask/metamask-plugin/issues/504
@@ -104,7 +106,7 @@ class App extends Component {
 
       // Check which network are we connected to
       // https://github.com/ethereum/meteor-dapp-wallet/blob/90ad8148d042ef7c28610115e97acfa6449442e3/app/client/lib/ethereum/walletInterface.js#L32-L46
-      if (this.state.isConnected !== isConnected) {
+      if (this.state.network.isConnected !== isConnected) {
         if (isConnected === true) {
           this.web3.eth.getBlock(0, (e, res) => {
             let network = false;
@@ -122,16 +124,16 @@ class App extends Component {
                   network = 'private';
               }
             }
-            if (this.state.network !== network) {
+            if (this.state.network.network !== network) {
               this.initNetwork(network);
             }
           });
         } else {
-          this.setState({
-            isConnected,
-            network: false,
-            latestBlock: 0
-          });
+          const networkState = {...this.state.network};
+          networkState['isConnected'] = isConnected;
+          networkState['network'] = false;
+          networkState['latestBlock'] = 0;
+          this.setState({ network: networkState });
         }
       }
     });
@@ -139,35 +141,35 @@ class App extends Component {
 
   initNetwork(newNetwork) {
     //checkAccounts();
-    this.setState({
-      network: newNetwork,
-      isConnected: true,
-      latestBlock: 0,
-    });
+    const networkState = {...this.state.network};
+    networkState['network'] = newNetwork;
+    networkState['isConnected'] = true;
+    networkState['latestBlock'] = 0;
+    this.setState({ network: networkState });
   }
 
   checkAccounts() {
     this.web3.eth.getAccounts((error, accounts) => {
       if (!error) {
-        this.setState({
-          accounts,
-          defaultAccount: accounts[0]
-        });
+        const networkState = {...this.state.network};
+        networkState['accounts'] = accounts;
+        networkState['defaultAccount'] = accounts[0];
+        this.setState({ network: networkState });
       }
     });
   }
 
   render() {
     return (
-      this.state.isConnected ? 
+      this.state.network.isConnected ? 
         <div className="App">
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h2>Simplecoin Explorer</h2>
           </div>
           {
-            Object.keys(this.state).map(key => <p key={key}>{key}:
-            &nbsp;{typeof(this.state[key]) === 'boolean' ? (this.state[key] ? 'true' : 'false') : this.state[key]}</p>)
+            Object.keys(this.state.network).map(key => <p key={key}>{key}:
+            &nbsp;{typeof(this.state.network[key]) === 'boolean' ? (this.state.network[key] ? 'true' : 'false') : this.state.network[key]}</p>)
           }
         </div>
       :
