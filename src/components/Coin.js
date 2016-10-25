a9d0e36ca2488180d8039c6b1791c7729f336a1c
gbalabasquer <gbalabasquer@gmail.com>
2016-10-25 15:54:55 -0300

    Settings values to specific coin

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,27 +3,22 @@ import React, { Component } from 'react';
 class Coin extends Component {
   constructor(props) {
     super();
-    const simplecoin = props.simplecoinFactory.classes.Simplecoin.at(props.index);
-    
+    this.simplecoin = props.simplecoinFactory.classes.Simplecoin.at(props.index);
+
     //Testing purpose
-    window.simplecoin = simplecoin;
+    window.simplecoin = this.simplecoin;
     //
 
-    const me = this;
-    simplecoin.owner((error, result) => {
-      me.props.updateCoin(me.props.index, 'owner', result);
-    });
-    simplecoin.rules((error, result) => {
-      me.props.updateCoin(me.props.index, 'rules', result);
-    });
-    simplecoin.feed((error, result) => {
-      me.props.updateCoin(me.props.index, 'feed', result);
-    });
-    simplecoin.spread((error, result) => {
-      me.props.updateCoin(me.props.index, 'spread', result);
-    });
-    simplecoin.totalSupply((error, result) => {
-      me.props.updateCoin(me.props.index, 'totalSupply', result);
+    this.getValueAndUpdate('owner');
+    this.getValueAndUpdate('rules');
+    this.getValueAndUpdate('feed');
+    this.getValueAndUpdate('spread');
+    this.getValueAndUpdate('totalSupply');
+  }
+
+  getValueAndUpdate(field) {
+    this.simplecoin[field]((error, result) => {
+      this.props.updateCoin(this.props.index, field, result);
     });
   }
   
