f210dced759b881525ee6c4bd62bf3ff2ae6b0fe
Mariano Conti <nanexcool@gmail.com>
2016-10-26 12:43:04 -0300

    Styling

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -9,9 +9,12 @@ const Coins = (props) => {
       </div>
       {
         Object.keys(props.coins).map(key => 
-          <div className="col-md-6 Coins-box" key={key}>
+          <div className="col-sm-6 Coins-box" key={key}>
             <p>
-              <a href={"#" + props.coins[key]['coinId']}>{props.coins[key]['coinId']}</a>
+              <a href={`#${props.coins[key]['coinId']}`}>{props.coins[key]['coinId']}</a>
+            </p>
+            <p>
+              Owner: {props.coins[key].owner || '(Pending...)'}
             </p>
           </div>
         )
