542b95c3a8864664464d5dcfc3cc48068b0915b9
Mariano Conti <nanexcool@gmail.com>
2016-10-27 11:59:20 -0300

    Changed row length

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -4,7 +4,7 @@ import web3 from '../web3';
 const Accounts = (props) => {
 
   return (
-    <div className="col-sm-6">
+    <div className="col-sm-12">
       <p>{web3.eth.accounts}</p>
       <p>{props.account}</p>
       <button onClick={() => props.changeAccount(web3.eth.accounts[0])}>Change Account</button>
