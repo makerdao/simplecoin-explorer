0dc48363966d143e9b72719fad8d4d67996d2851
Mariano Conti <nanexcool@gmail.com>
2016-11-01 14:07:23 -0300

    Navbar green dot

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,17 @@ import './NavBar.css';
 import logo from '../logo.svg';
 
 const NavBar = (props) => {
-  const text = props.isConnected ? `Connected to ${props.network}` : 'No connection';
+  const text = props.isConnected ? `${props.network}` : 'No connection';
   return (
     <nav className="navbar navbar-inverse NavBar">
       <div className="container">
           <a className="navbar-brand" href="#" onClick={(e) => props.setUrl('')}>
             <img className="NavBar-logo" alt="Brand" src={logo} />
           </a>
-        <p className="navbar-text"><strong>{text}</strong></p>
+        <p className="navbar-text">
+          <span className="glyphicon glyphicon-one-fine-green-dot"></span>
+          <strong>{text}</strong>
+        </p>
       </div>
     </nav>
   )
